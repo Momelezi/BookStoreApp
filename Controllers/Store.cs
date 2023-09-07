7d9b0af2372d0edfb179a4144a76106665dcdc4a
Momelezi <momeleziqaana97@gmail.com>
2023-09-07 13:42:14 +0200

    Sore

---
+++ Controllers/Store.cs
@@ -0,0 +1,44 @@
+﻿using BookStoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Controllers
+{
+    
+    public class Store : Controller
+    {
+        private readonly BookStoreAppContext _context;
+
+        public Store(BookStoreAppContext context)
+        {
+            _context = context;
+        }
+
+        //Get Books
+        public async Task<IActionResult> Index()
+        {
+            return _context.Books != null ?
+                        View(await _context.Books.ToListAsync()) :
+                        Problem("Entity set 'BookStoreAppContext.Book'  is null.");
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+    }
+}
